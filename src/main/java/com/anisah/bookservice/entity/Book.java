98b0aff3831e8bc7a9e0a40b8b428e932a927b68
Anisah <nisaanisah1220@gmail.com>
2026-02-22 14:53:12 +0700

    add Book entity and BookRepository interface

---
+++ src/main/java/com/anisah/bookservice/entity/Book.java
@@ -0,0 +1,29 @@
+package com.anisah.bookservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    private LocalDate publishedDate;
+}
